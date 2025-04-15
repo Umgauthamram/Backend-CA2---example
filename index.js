7afc7bab24bfc718b679021c56d72612f1ddb5e4
Gautham Ram <gauthamram.um@gmail.com>
2025-04-15 08:13:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 
+app.get("/", (req,res)=>{
+    res.send("<h1>Hello, server is running.</h1>")
+})
 
 app.post("/post", async (req, res) => {
   try {
